6115bfe69d1bc96cdaa624996a5e7ad3d27c7b4f
saur98 <saurabh.maheshwari1011@gmail.com>
2022-07-27 00:02:05 +0530

    removed extra code

--- client/src/routes/Trends.js
+++ client/src/routes/Trends.js
@@ -1,56 +1,51 @@
 import axios from 'axios'
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import '.././App.css';
 
 
-function Trends(props){
-    const [searches,setSearches] = useState([])
-    const [content,setContent] = useState([])
+function Trends(props) {
+    const [searches, setSearches] = useState([])
+    const [content, setContent] = useState([])
     useEffect(() => {
         const URL = "/api/dailytrends"
         const config = {
-            onDownloadProgress : function(progressEvent){
-                
+            onDownloadProgress: function (progressEvent) {
                 setContent(progressEvent.srcElement.response)
             }
         }
-        
-        axios.get(URL).then( data => {
+
+        axios.get(URL).then(data => {
             console.log(data)
             setSearches(data)
-            axios.post("/api/link",{trendingSearch:data.data} ,config).then(data=> {
-                const {values} = data
-                console.log(values)
-                
-            })   
+            axios.post("/api/link", { trendingSearch: data.data }, config)
         })
-        .catch( err => console.log(err))
+            .catch(err => console.log(err))
 
-        
-      
 
-    },[])
 
-    function displaysearch(){ 
-            if(searches.data)
+
+    }, [])
+
+    function displaysearch() {
+        if (searches.data)
             return searches.data.map(data => {
-                return <Today value={data}/>
-            })       
+                return <Today value={data} />
+            })
 
-        }
+    }
 
-    
-    return(
+
+    return (
         <>
-        {displaysearch()}
-        <div className="content" >{content}</div>
+            {displaysearch()}
+            <div className="content" >{content}</div>
         </>
     )
 
 }
 
-function Today(props){
-    const {dateformatted,trendingSearches} = props.value
+function Today(props) {
+    const { dateformatted, trendingSearches } = props.value
     //console.log(dateformatted)
     const date = () => {
         return dateformatted
@@ -62,8 +57,8 @@ function Today(props){
 
     return (
         <>
-        <h1>{date()}</h1>
-        <h6 className="searches">{values()}</h6>
+            <h1>{date()}</h1>
+            <h6 className="searches">{values()}</h6>
         </>
     )
 }
--- routes/google-trends.js
+++ routes/google-trends.js
@@ -9,63 +9,56 @@ const cheerio = require('cheerio')
 const app = express()
 app.use(fileUpload());
 
-app.get("/api/dailytrends",async (request,response) => {
+app.get("/api/dailytrends", async (request, response) => {
     const data = await googleTrends.dailyTrends({ geo: "US" })
     console.log(typeof data)
     const searches = []
     const datas = JSON.parse(data).default.trendingSearchesDays
-    trendingSearchesDays = datas.map(data => {     
-        return {date:data.date,dateformatted:data.formattedDate,trendingSearches:data.trendingSearches}
+    trendingSearchesDays = datas.map(data => {
+        return { date: data.date, dateformatted: data.formattedDate, trendingSearches: data.trendingSearches }
     })
     const trendingSearches = trendingSearchesDays.map(data => {
-        for(key in data){
-                if(key==='trendingSearches')
-                {
-                    data[key] = data[key].map(data => {
-                        return {query:data.title.query,URL:data.image.newsUrl}
-                    })
-                }
+        for (key in data) {
+            if (key === 'trendingSearches') {
+                data[key] = data[key].map(data => {
+                    return { query: data.title.query, URL: data.image.newsUrl }
+                })
+            }
         }
         return data
     })
-    
+
     response.status(200).json(trendingSearches)
-    
+
 });
 
-app.post("/api/link",async (request,response) => {
-    //console.log(request.body.trendingSearch)
+app.post("/api/link", async (request, response) => {
     const trendingSearch = request.body.trendingSearch
-    try{
-   
-    
-    for (i in trendingSearch){
-        for(j in trendingSearch[i]['trendingSearches']){
-            let values = []
-            const URL = trendingSearch[i]['trendingSearches'][j].URL
-            console.log(URL)
-            const browser = await puppeteer.launch().catch(err => console.log)
-            const page = await browser.newPage().catch(err => console.log)
-    await page.goto(URL,{waitUntil : 'domcontentloaded'}).catch(err => console.log)
-    const pTags = await page.$$("p").catch(err => console.log)
-    
-    if(pTags.length > 0){
-    
-    for(ptag in pTags)
-    {   
-        let value = await pTags[ptag].evaluate(el => el.textContent).catch(err => console.log)
-        values.push(value)
+    try {
+        for (i in trendingSearch) {
+            for (j in trendingSearch[i]['trendingSearches']) {
+                let values = []
+                const URL = trendingSearch[i]['trendingSearches'][j].URL
+                const browser = await puppeteer.launch().catch(err => console.log)
+                const page = await browser.newPage().catch(err => console.log)
+                await page.goto(URL, { waitUntil: 'domcontentloaded' }).catch(err => console.log)
+                const pTags = await page.$$("p").catch(err => console.log)
+
+                if (pTags.length > 0) {
+
+                    for (ptag in pTags) {
+                        let value = await pTags[ptag].evaluate(el => el.textContent).catch(err => console.log)
+                        values.push(value)
+                    }
+                }
+                response.write(values.toString())
+                browser.close()
+            }
+
+        }
+
     }
-    }
-    //console.log(values)
-    response.write(values.toString())
-    browser.close()
-    }
-    
-    }
-    
-    }
-    catch(err){console.log(err)}
+    catch (err) { console.log(err) }
     response.end()
 })
 
