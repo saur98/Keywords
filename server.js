9ca575e8d14f1f3e7ab68a6d26b5fa29ff133379
saur98 <saurabh.maheshwari1011@gmail.com>
2022-08-15 22:14:00 +0530

    without auto setup

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(compression())
-checkDir()
+//checkDir()
 connectDB()
-Populate()
+//Populate()
 app.use(express.json({limit: '50mb'}))
 
 app.use(trends)
